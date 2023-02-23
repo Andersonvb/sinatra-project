b8e4077d6387681cfe7fa41db8c1430ab51aaf5e
Andersonvb <andersonvalenciabermudez@gmail.com>
2023-02-23 13:58:33 -0500

    Fix some bugs

--- app.rb
+++ app.rb
@@ -3,14 +3,27 @@
 require 'sinatra'
 require_relative './models/task'
 
+# Index
 get '/' do
   @tasks = Task.all
-  #@tasks = []
   erb :index
 end
 
-post '/' do
-  task = Task.new({ content: params[:content], completed: false })
+# Create
+post '/tasks' do
+  task = Task.new({ 'content' => params[:content], 'completed' => false })
   task.save
+  redirect '/'
+end
+
+# Delete
+get '/delete/:id' do
+  @task = Task.find(params[:id])
+  erb :'tasks/delete'
+end
+
+post '/tasks/:id' do
+  @task = Task.find(params[:id])
+  @task.destroy
   redirect '/'
 end
--- models/task.rb
+++ models/task.rb
@@ -4,18 +4,18 @@ require 'json'
 require 'net/http'
 
 class Task
-  URL = 'https://crudcrud.com/api/42f72cf27c2148788ccdc5df3f3529b5/tasks'
+  BASE_URL = 'https://crudcrud.com/api/71313388b24a495abc280a6e55de854c/tasks'
 
   attr_accessor :id, :content, :completed
 
   def initialize(params = {})
-    @id = params[:id]
-    @content = params[:content]
-    @completed = params[:completed]
+    @id = params['_id']
+    @content = params['content']
+    @completed = params['completed']
   end
 
   def save
-    uri = URI(URL)
+    uri = URI(BASE_URL)
     headers = { 'Content-Type': 'application/json' }
     task = {
       content: content,
@@ -26,30 +26,36 @@ class Task
       response = Net::HTTP.post(uri, task, headers)
       @id = JSON.parse(response.body)['_id']
     else
-      uri = URI("#{URL}/#{id}")
+      uri = URI("#{BASE_URL}/#{id}")
       Net::HTTP.put(uri, task, headers)
     end
   end
 
   def self.find(id)
-    uri = URI("#{URL}/#{id}")
+    uri = URI("#{BASE_URL}/#{id}")
     response = Net::HTTP.get(uri)
 
-    Task.new(JSON.parse(response.body))
+    Task.new(JSON.parse(response))
   end
 
   def self.all
-    uri = URI(URL)
+    uri = URI(BASE_URL)
     response = Net::HTTP.get(uri)
     tasks = JSON.parse(response)
 
     tasks.map { |task| Task.new(task) }
   end
 
+  # TODO: Arreglar este bug, no elimina esa cosa
   def destroy
-    uri = URI("#{URL}/#{id}")
-    response = Net::HTTP.delete(uri)
+    uri = URI("#{BASE_URL}/#{id}")
+    request = Net::HTTP::Delete.new(uri)
 
-    response == 204
+    # Se usa Net::HTTP.start para enviar la solicitud a traves de HTTP
+    response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(request)
+    end 
+
+    puts "RESPONSEEEEEE #{response}" 
   end
 end
